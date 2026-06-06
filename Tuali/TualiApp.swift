714d88ef8d49872d199fc76bc97c027253c14e20
Angel HG <angel.hdz.g@icloud.com>
2026-06-06 14:57:37 -0600

    Initial Commit

---
+++ Tuali/TualiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TualiApp.swift
+//  Tuali
+//
+//  Created by Angel HG on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TualiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
